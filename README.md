a31807fd1302f1b57dbe389dd6cfd709febb0667
REEEM96 <Reem.Shihab@edu.futuregames.se>
2025-10-27 19:09:09 +0100

    coomit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Labbgrund till Moment 5 i kursen DT084G, Introduktion till programmering i JavaScript
+ hej
---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Denna fil ska innehålla din lösning till uppgiften (moment 5).
+
+"use strict";
+
+/*  Delar till ej obligatorisk funktionalitet, som kan ge poäng för högre betyg
+*   Radera rader för funktioner du vill visa på webbsidan. */
+document.getElementById("player").style.display = "none";      // Radera denna rad för att visa musikspelare
+document.getElementById("shownumrows").style.display = "none"; // Radera denna rad för att visa antal träffar
+
+/* Här under börjar du skriva din JavaScript-kod */
