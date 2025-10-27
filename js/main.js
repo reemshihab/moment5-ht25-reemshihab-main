6d3926d46ba3f2691cfb7adcd1aa4b4f34df485e
REEEM96 <Reem.Shihab@edu.futuregames.se>
2025-10-27 21:03:49 +0100

    change

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // Denna fil ska innehålla din lösning till uppgiften (moment 5).
 
-"use strict";
+"use strict"; 
 
 /*  Delar till ej obligatorisk funktionalitet, som kan ge poäng för högre betyg
 *   Radera rader för funktioner du vill visa på webbsidan. */
